291f2d93eadecf70bdf0080c957f21e012a38e2f
Brett Coetzee <brett.coetzee@stratusolve.com>
2020-01-05 18:24:51 +0200

    Foundation Task 4

--- index.php
+++ index.php
@@ -7,30 +7,10 @@
 class ItemOwners {
   public static function groupByOwners($items) {
     $group = array();
-    ItemOwners::echoAssArray($items);
     foreach($items as $key => $value){
-      $group[$value][] = $key; // Reverse key->value
-    } 
-    ItemOwners::echoMultiAssArray($group);
+      $group[$value][] = $key;
+    }
     return $group;
-  }
-  
-  // Note that Training is not code golf, thus fundamental array reading basics included below
-  public static function echoAssArray($assArray) {
-    echo "Key-Value-Pair Array: ". json_encode($assArray) ."<br>";
-    foreach($assArray as $key => $value){
-      echo "- " . $key . ": " . $value . "<br>";
-    }
-  }
-  public static function echoMultiAssArray($multiAssArray) {
-    echo "Key-Values-Pair (Multidimensional Associative) Array: ". json_encode($multiAssArray) ."<br>";
-    foreach($multiAssArray as $key => $values){
-      echo "- " . $key . ": ";
-      foreach ($values as $value) {
-        echo "'" . $value . "' ";
-      }
-      echo "<br>";
-    }
   }
 }
 $items = array(
